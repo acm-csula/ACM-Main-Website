ef6669ff430b843a2fc2be2022106401ad2e8361
LudiJolo <joloh18@yahoo.com>
2023-12-21 14:59:32 -0800

    fetch data from firestore
    - finished view modal
    - currently working on re-rendering list after deletion

--- src/components/admin/eventsAdmin/EventsAdmin.js
+++ src/components/admin/eventsAdmin/EventsAdmin.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card, Tab, Tabs, Row, Button } from "react-bootstrap";
 import img from "./IMG_0118.png";
 import EventSubTab from "./event-subtab";
 import AddModal from "./modals/addModal";
 import './eventsadmin.css'
+import { db } from "../../professional-events/firebaseConfig";
+import { collectionGroup, getDocs } from "firebase/firestore";
 const dummyUpcoming = [
     {
       altText: "Event name",
@@ -42,11 +44,49 @@ const dummyUpcoming = [
 
 
 const EventsAdmin = () => {
-    const [activeTab, setActiveTab] = useState('upcoming');//currently selected tab
+    const [activeTab, setActiveTab] = useState('upcomingEvents');//currently selected tab
     const [addModal, setAddModal] = useState(false);
     const [upcoming, setUpcoming] = useState([]);
     const [semester, setSemester] = useState([]);
     const [past, setPast] = useState([]);
+  
+
+    //initialization
+    //fetch events from firestore collection: upcomingEvents, semesterEvents, pastEvents
+    useEffect(() => {
+      (async () => {
+        try {
+          const upcoming = collectionGroup(db, "upcomingEvents");
+          const semester = collectionGroup(db, "semesterEvents");
+          const upcomingSnapshot = await getDocs(upcoming);
+          const semSnapshot = await getDocs(semester);
+          const upcomingGroup = [];
+          const semGroup = [];
+          upcomingSnapshot.forEach((doc) => {
+            const uEvent = {};
+            uEvent['id'] = doc.id;
+            uEvent['altText'] = doc.data().altText;
+            uEvent['imgUrl'] = doc.data().imgUrl;
+            upcomingGroup.push(uEvent);
+          });
+          semSnapshot.forEach((doc) => {
+            const sEvent = {};
+            sEvent['id'] = doc.id;
+            sEvent['altText'] = doc.data().altText;
+            sEvent['imgUrl'] = doc.data().imgUrl;
+            semGroup.push(sEvent);
+          });
+  
+          setUpcoming(upcomingGroup);
+          setSemester(semGroup);
+          console.log(upcomingGroup);
+          console.log(semGroup);
+        } catch (err) {
+          console.log("Error occured when fetching events");
+        }
+      })();
+    }, []);
+
 
     //sets the current selected tab
     const handleTabSelect = (selectedKey) => {
@@ -61,14 +101,14 @@ const EventsAdmin = () => {
       <Button className="mx-auto mb-3" onClick={() => setAddModal(true)}>Add an event</Button>
       <AddModal show={addModal} onHide={() => setAddModal(false)} currentTab={activeTab} />
       <Tabs defaultActiveKey={activeTab} onSelect={handleTabSelect} id="sub-tabs" className="mb-3 event-tabs">
-        <Tab eventKey="upcoming" title="Upcoming">
-          <EventSubTab data={dummyUpcoming} />
+        <Tab eventKey="upcomingEvents" title="Upcoming">
+          <EventSubTab data={upcoming} activeSection={activeTab} />
         </Tab>
-        <Tab eventKey="semester" title="Semester">
-          <EventSubTab data={dummySemester} />
+        <Tab eventKey="semesterEvents" title="Semester">
+          <EventSubTab data={semester} activeSection={activeTab} />
         </Tab>
-        <Tab eventKey="past" title="Past">
-          <EventSubTab data={dummyPast} />
+        <Tab eventKey="pastEvents" title="Past">
+          <EventSubTab data={dummyPast} activeSection={activeTab}/>
         </Tab>
       </Tabs>
     </div>
