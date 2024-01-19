9db134a22233154f70015dc132a490051479ea6a
LudiJolo <joloh18@yahoo.com>
2024-01-19 14:11:58 -0800

    Hidden code cracking

--- src/components/navbar/Navi.js
+++ src/components/navbar/Navi.js
@@ -109,10 +109,12 @@ class Navi extends React.Component {
                   <div className="nav-dropdown-text">project workshops</div>
                 </NavDropdown.Item>
 
+                {/*
                 
                 <NavDropdown.Item href="/programs">
                   <div className="nav-dropdown-text">code cracking</div>
                 </NavDropdown.Item>
+                */}
 
                 <NavDropdown.Item href="/professional">
                   <div className="nav-dropdown-text">events</div>
